0b054684ec4d66a96e9cad8fbebc1976ed8d6d7c
legend <tomybryant1@gmail.com>
2024-03-24 16:05:54 +0000

    "bytes error corrected"

--- Voting.sol
+++ Voting.sol
@@ -1,24 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract Voting{
+contract Voting {
+    struct Proposal {
+        address target;
+        bytes data;
+        uint yesCount;
+        uint noCount;
+    }
 
- struct Proposal{
-  bytes data,
-  address target,
-  uint yesCount,
-  uint noCount
- }
+    Proposal[] public proposals;
 
- Proposal[] public proposals
-
- function newProposal(address _target, bytes calldata _data) external{
-    Proposal memory newProposalInstance = Proposal({
-        yesCount:0,
-        noCount:0,
-        data: _data,
-        target: _target
-    })
-    proposals.push(newProposalInstance);
- }
+    function newProposal(address _target, bytes calldata _data) external {
+        Proposal memory newProposalInstance = Proposal({
+            target: _target,
+            data: _data,
+            yesCount: 0,
+            noCount: 0
+        });
+        proposals.push(newProposalInstance);
+    }
 }
